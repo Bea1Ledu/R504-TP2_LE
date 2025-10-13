a488b92717ebc86254400ff268f4c30954f05e8d
beatrice <beatricele12@gmail.com>
2025-10-13 14:05:11 +0200

    feat(fizzbuzz A):modification 1

---
+++ src/fizzbuzz.py
@@ -0,0 +1,14 @@
+
+def _token(n: int) -> str:
+    if n % 15 == 0:
+        return "FrisBee"
+    if n % 3 == 0:
+        return "Fizz"
+    if n % 5 == 0:
+        return "Buzz"
+    return str(n)
+
+def affiche():
+    s = "".join(_token(i) for i in range(1, 101))
+    print(s, end="")
+
