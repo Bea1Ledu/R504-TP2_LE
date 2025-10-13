6a9cc9efea762bba9d4652c083be0e2ac2f36708
beatrice <beatricele12@gmail.com>
2025-10-13 13:46:12 +0200

    test(fizzbuzz question A)

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,25 @@
+
+from io import StringIO
+
+from contextlib import redirect_stdout
+
+from src.fizzbuzz import affiche
+
+def capture_output(func):
+
+    buf = StringIO()
+
+    with redirect_stdout(buf):
+
+        func()
+
+    return buf.getvalue()
+
+def test_affiche_sans_param_remplace_multiples():
+
+    out = capture_output(affiche)
+
+    assert out.startswith("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
+
+    assert out.endswith("9798FizzBuzz")
+
